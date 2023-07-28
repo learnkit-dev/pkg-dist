8534e77f94d4fc9c8326fc4e28f28ee2ce1a7880
Sebastiaan Kloos <sebastiaan@codecycler.com>
2023-07-28 11:37:01 +0200

    wip

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,14 @@
 
 namespace App\Providers;
 
+use App\Models\License;
+use App\Models\Package;
+use App\Models\Team;
+use App\Models\User;
+use App\Models\Version;
 use Illuminate\Support\ServiceProvider;
+use Statview\Satellite\Statview;
+use Statview\Satellite\Widgets\Widget;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +26,26 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Statview::registerWidgets([
+            Widget::make('users')
+                ->title('# users')
+                ->value(User::count()),
+
+            Widget::make('teams')
+                ->title('# teams')
+                ->value(Team::count()),
+
+            Widget::make('packages')
+                ->title('# packages')
+                ->value(Package::count()),
+
+            Widget::make('versions')
+                ->title('# versions')
+                ->value(Version::count()),
+
+            Widget::make('licenses')
+                ->title('# licenses')
+                ->value(License::count()),
+        ]);
     }
 }
